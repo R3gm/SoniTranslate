d73cb7bb8a14215bd336ecc2e6160fcaff523733
Roger Condori <114810545+R3gm@users.noreply.github.com>
2023-07-08 10:56:19 -0400

    #place  lang  translate_segments.py

--- soni_translate/translate_segments.py
+++ soni_translate/translate_segments.py
@@ -3,10 +3,10 @@ from deep_translator import GoogleTranslator
 
 def translate_text(segments, TRANSLATE_AUDIO_TO):
 
-    translator = GoogleTranslator(source='auto', target=TRANSLATE_AUDIO_TO)
-
     if TRANSLATE_AUDIO_TO == "zh":
         TRANSLATE_AUDIO_TO = "zh-CN"
+    
+    translator = GoogleTranslator(source='auto', target=TRANSLATE_AUDIO_TO)
     
     for line in tqdm(range(len(segments))):
         text = segments[line]['text']
