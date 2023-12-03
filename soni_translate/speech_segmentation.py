cd62e69d0b35d242f8fbe9a34b9803d64b1bd8b1
Roger Condori <114810545+R3gm@users.noreply.github.com>
2023-12-03 19:23:52 -0400

    Added documentation, alignment models

--- soni_translate/speech_segmentation.py
+++ soni_translate/speech_segmentation.py
@@ -1,11 +1,26 @@
 from whisperx.alignment import DEFAULT_ALIGN_MODELS_TORCH as DAMT, DEFAULT_ALIGN_MODELS_HF as DAMHF
 import whisperx, torch, gc
 from IPython.utils import capture
-
+from .language_configuration import EXTRA_ALIGN
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 def transcribe_speech(audio_wav, WHISPER_MODEL_SIZE, compute_type, batch_size, SOURCE_LANGUAGE):
+    """
+    Transcribe speech using a whisper model.
+
+    Parameters:
+    - audio_wav (str): Path to the audio file in WAV format.
+    - WHISPER_MODEL_SIZE (str): The whisper model to be loaded.
+    - compute_type (str): Type of compute to be used (e.g., 'int8', 'float16').
+    - batch_size (int): Batch size for transcription.
+    - SOURCE_LANGUAGE (str): Source language for transcription.
+
+    Returns:
+    - Tuple containing:
+        - audio: Loaded audio file.
+        - result: Transcription result as a dictionary.
+    """
     with capture.capture_output() as cap:
         model = whisperx.load_model(
             WHISPER_MODEL_SIZE,
@@ -16,23 +31,32 @@ def transcribe_speech(audio_wav, WHISPER_MODEL_SIZE, compute_type, batch_size, S
         del cap
     audio = whisperx.load_audio(audio_wav)
     result = model.transcribe(audio, batch_size=batch_size)
-    gc.collect(); torch.cuda.empty_cache(); del model
+    del model; gc.collect(); torch.cuda.empty_cache()
     return audio, result
 
 
 def align_speech(audio, result):
-    # 2. Align whisper output
+    """
+    Aligns speech segments based on the provided audio and result metadata.
 
+    Parameters:
+    - audio (array): The audio data in a suitable format for alignment.
+    - result (dict): Metadata containing information about the segments and language.
+
+    Returns:
+    - result (dict): Updated metadata after aligning the segments with the audio.
+      This includes character-level alignments if 'return_char_alignments' is set to True.
+
+    Notes:
+    - This function uses language-specific models to align speech segments.
+    - It performs language compatibility checks and selects the appropriate alignment model.
+    - Cleans up memory by releasing resources after alignment.
+    """
     DAMHF.update(DAMT) #lang align
-    EXTRA_ALIGN = {
-        "id": "indonesian-nlp/wav2vec2-large-xlsr-indonesian",
-    } # add new align models here
-    #print(result['language'], DAM.keys(), EXTRA_ALIGN.keys())
     if not result['language'] in DAMHF.keys() and not result['language'] in EXTRA_ALIGN.keys():
         audio = result = None
-        print("Automatic detection: Source language not compatible")
-        print(f"Detected language {result['language']}  incompatible, you can select the source language to avoid this error.")
-        return
+        print("Automatic detection: Source language not compatible with align")
+        raise ValueError(f"Detected language {result['language']}  incompatible, you can select the source language to avoid this error.")
 
     model_a, metadata = whisperx.load_align_model(
         language_code=result["language"],
@@ -47,7 +71,7 @@ def align_speech(audio, result):
         device,
         return_char_alignments=True,
         )
-    gc.collect(); torch.cuda.empty_cache(); del model_a
+    del model_a; gc.collect(); torch.cuda.empty_cache()
     return result
 
 diarization_models = {
@@ -56,15 +80,49 @@ diarization_models = {
 }
 
 def diarize_speech(audio_wav, result, min_speakers, max_speakers, YOUR_HF_TOKEN, model_name="pyannote/speaker-diarization@2.1"):
-    # 3. Assign speaker labels
-    with capture.capture_output() as cap:
-        diarize_model = whisperx.DiarizationPipeline(model_name=model_name, use_auth_token=YOUR_HF_TOKEN, device=device)
-        del cap
-    diarize_segments = diarize_model(
-        audio_wav,
-        min_speakers=min_speakers,
-        max_speakers=max_speakers)
+    """
+    Performs speaker diarization on speech segments.
 
-    result_diarize = whisperx.assign_word_speakers(diarize_segments, result)
-    gc.collect(); torch.cuda.empty_cache(); del diarize_model
+    Parameters:
+    - audio_wav (array): Audio data in WAV format to perform speaker diarization.
+    - result (dict): Metadata containing information about speech segments and alignments.
+    - min_speakers (int): Minimum number of speakers expected in the audio.
+    - max_speakers (int): Maximum number of speakers expected in the audio.
+    - YOUR_HF_TOKEN (str): Your Hugging Face API token for model authentication.
+    - model_name (str): Name of the speaker diarization model to be used (default: "pyannote/speaker-diarization@2.1").
+
+    Returns:
+    - result_diarize (dict): Updated metadata after assigning speaker labels to segments.
+
+    Notes:
+    - This function utilizes a speaker diarization model to label speaker segments in the audio.
+    - It assigns speakers to word-level segments based on diarization results.
+    - Cleans up memory by releasing resources after diarization.
+    - If only one speaker is specified, each segment is automatically assigned as the first speaker, eliminating the need for diarization inference.
+    """
+    if max(min_speakers, max_speakers) > 1:
+        try:
+            with capture.capture_output() as cap:
+                diarize_model = whisperx.DiarizationPipeline(model_name=model_name, use_auth_token=YOUR_HF_TOKEN, device=device)
+                del cap
+        except Exception as error:
+            error = str(error)
+            gc.collect(); torch.cuda.empty_cache()
+            if "'NoneType' object has no attribute 'to'" in error:
+                if model_name == diarization_models["pyannote_2.1"]:
+                    raise ValueError("Accept the license agreement for using Pyannote 2.1. You need to have an account on Hugging Face and accept the license to use the models: https://huggingface.co/pyannote/speaker-diarization and https://huggingface.co/pyannote/segmentation. Get your KEY TOKEN here: https://hf.co/settings/tokens")
+                elif model_name == diarization_models["pyannote_3.1"]:
+                    raise ValueError("New Licence Pyannote 3.1: You need to have an account on Hugging Face and accept the license to use the models: https://huggingface.co/pyannote/speaker-diarization-3.0 and https://huggingface.co/pyannote/segmentation-3.0.")
+            else:
+                raise error
+        diarize_segments = diarize_model(
+            audio_wav,
+            min_speakers=min_speakers,
+            max_speakers=max_speakers)
+
+        result_diarize = whisperx.assign_word_speakers(diarize_segments, result)
+        del diarize_model; gc.collect(); torch.cuda.empty_cache()
+    else:
+        result_diarize = result
+        result_diarize["segments"] = [{**item, "speaker": "SPEAKER_00"} for item in result_diarize["segments"]]
     return result_diarize
